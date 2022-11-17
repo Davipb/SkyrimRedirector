f4fb8c74c76bd7fea9912669637367c7d8220468
Heather Anderson <heath@odysseus.anderson.name>
2022-11-16 22:53:11 -0800

    adding new version declaration that newer SKSE64 versions require (mostly saying "just let us run")

--- SkyrimRedirector/Main.c
+++ SkyrimRedirector/Main.c
@@ -7,6 +7,24 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+__declspec(dllexport) SKSEPluginVersionData SKSEPlugin_Version =
+{
+	PLUGIN_INFO_VERSION,
+
+	1,
+	"Skyrim Redirector",
+
+	"",
+	"https://github.com/Davipb/SkyrimRedirector",
+
+	// setting ourselves as "not using structures" hopefully exempts us from any version checking
+	VERSIONINDEPENDENTEX_NO_STRUCT_USE,
+	VERSIONINDEPENDENT_SIGNATURES,
+	{ 0 },
+	0,
+};
+
+// API for compatibility with older versions of SKSE64
 __declspec(dllexport) bool SKSEPlugin_Query(const SKSEInterface* skse, PluginInfo* info)
 {
 	SR_DEBUG("SKSE query received");
